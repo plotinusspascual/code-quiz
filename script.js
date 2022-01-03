0887d83731232905c5f023a9e1d95075aacce7b0
plotinusspascual <plotinusspascual@gmail.com>
2022-01-03 15:53:05 -0800

    added starter function in js

---
+++ script.js
@@ -0,0 +1,20 @@
+const startButton = document.getElementById('start-button');
+const nextButton = document.getElementById('next-button');
+
+var choices = document.getElementById("")
+
+
+function startQuiz(){
+
+}
+
+
+const questions = [
+  {
+    question: 'What is 2+2?',
+    answers: [
+      { text: '4', correct: true },
+      { text: '1', correct: false}
+    ]
+  }
+]
